3486181dc016776955ea689d8899769a7efdca86
Paul Osborne <hello@paulosborne.me.uk>
2014-01-11 15:36:29 +0000

    Added basic configuration for client.

--- src/groovy/grails/plugin/heartinternet/resellerapi/EppClient.groovy
+++ src/groovy/grails/plugin/heartinternet/resellerapi/EppClient.groovy
@@ -2,4 +2,9 @@ package grails.plugin.heartinternet.resellerapi
 
 class EppClient {
 
+	def host
+	def port
+	def clID
+	def pw
+
 }
--- test/unit/grails/plugin/heartinternet/resellerapi/EppClientSpec.groovy
+++ test/unit/grails/plugin/heartinternet/resellerapi/EppClientSpec.groovy
@@ -4,8 +4,19 @@ import grails.plugin.spock.UnitSpec
 
 class EppClientSpec extends UnitSpec {
 
-	def "first test"() {
-		expect: true
+	def "it checks that config has been completed"() {
+		when: "initialising with values"
+			def theClient = new EppClient(
+				host:   "api.somehost.co.uk",
+				port:   1701,
+				clID:   "clientId",
+				pw:     "aPassword"
+			)
+		then: "the values are correctly set"
+			theClient.host  == "api.somehost.co.uk"
+			theClient.port  == 1701
+			theClient.clID  == "clientId"
+			theClient.pw    == "aPassword"
 	}
 
 }
