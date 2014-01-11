51d10b6d2f90ec136d705b744fb66c6362b9bf92
Paul Osborne <hello@paulosborne.me.uk>
2014-01-11 16:16:27 +0000

    Added ability to open a stream.

--- src/groovy/grails/plugin/heartinternet/resellerapi/EppClient.groovy
+++ src/groovy/grails/plugin/heartinternet/resellerapi/EppClient.groovy
@@ -1,10 +1,18 @@
 package grails.plugin.heartinternet.resellerapi
 
+import javax.net.ssl.SSLSocketFactory
+
 class EppClient {
 
 	def host
 	def port
 	def clID
 	def pw
+
+	Socket stream
+
+	void openStream() {
+		stream = SSLSocketFactory.default.createSocket(host, port)
+	}
 
 }
--- test/unit/grails/plugin/heartinternet/resellerapi/EppClientSpec.groovy
+++ test/unit/grails/plugin/heartinternet/resellerapi/EppClientSpec.groovy
@@ -4,19 +4,43 @@ import grails.plugin.spock.UnitSpec
 
 class EppClientSpec extends UnitSpec {
 
+	def dummyClient
+
+	def setup() {
+		dummyClient = new EppClient(
+			host:   "api.heartinternet.co.uk",
+			port:   1701,
+			clID:   "a0aa1234567aa998",
+			pw:     "+epERjmIBb"
+		)
+	}
+
 	def "it checks that config has been completed"() {
 		when: "initialising with values"
 			def theClient = new EppClient(
 				host:   "api.somehost.co.uk",
-				port:   1701,
+				port:   1234,
 				clID:   "clientId",
 				pw:     "aPassword"
 			)
 		then: "the values are correctly set"
 			theClient.host  == "api.somehost.co.uk"
-			theClient.port  == 1701
+			theClient.port  == 1234
 			theClient.clID  == "clientId"
 			theClient.pw    == "aPassword"
+	}
+
+	def "it creates a stream correctly"() {
+
+		when: "opening a stream"
+			dummyClient.openStream()
+			def theStream = dummyClient.stream
+
+		then: "the stream is correct"
+			theStream != null
+			theStream.host == dummyClient.host
+			theStream.port == dummyClient.port
+
 	}
 
 }
