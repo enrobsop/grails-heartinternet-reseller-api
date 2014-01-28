5300af65b3c5a026f4887706d871a245e289c65a
Paul Osborne <hello@paulosborne.me.uk>
2014-01-28 15:18:51 +0000

    Version 0.1.1.

--- HeartInternetResellerApiGrailsPlugin.groovy
+++ HeartInternetResellerApiGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class HeartInternetResellerApiGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.1.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
